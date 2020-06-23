631ff8b59c4957e658e56d9ea8ed9919bca7fcc7
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2020-06-23 15:50:23 +0900

    Testing with Junit
    test out project with Junit

---
+++ src/test/java/com/example/hangman/GameActivityTest.java
@@ -0,0 +1,22 @@
+package com.example.hangman;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameActivityTest extends GameActivity {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testOnCreate() {
+    }
+}
---
+++ src/test/java/com/example/hangman/TutorialActivityTest.java
@@ -0,0 +1,26 @@
+package com.example.hangman;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TutorialActivityTest extends TutorialActivity {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testOnCreate() {
+    }
+
+    @Test
+    public void testMOnClick() {
+    }
+}
